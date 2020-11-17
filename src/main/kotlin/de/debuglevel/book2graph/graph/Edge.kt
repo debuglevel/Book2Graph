45034261d920e1511968cc85e35164c0cb99abd0
Marc Kohaupt <debuglevel@gmail.com>
2020-11-17 21:48:56 +0100

    Formatting

--- src/main/kotlin/de/debuglevel/book2graph/graph/Edge.kt
+++ src/main/kotlin/de/debuglevel/book2graph/graph/Edge.kt
@@ -1,5 +1,8 @@
 package de.debuglevel.book2graph.graph
 
-data class Edge<T>(val start: Vertex<T>, val end: Vertex<T>) {
+data class Edge<T>(
+    val start: Vertex<T>,
+    val end: Vertex<T>
+) {
     override fun toString() = "$start -> $end"
 }
--- src/main/kotlin/de/debuglevel/book2graph/graph/Vertex.kt
+++ src/main/kotlin/de/debuglevel/book2graph/graph/Vertex.kt
@@ -1,6 +1,11 @@
 package de.debuglevel.book2graph.graph
 
-data class Vertex<T>(val content: T, val color: Color, val shape: Shape, val tooltip: String) {
+data class Vertex<T>(
+    val content: T,
+    val color: Color,
+    val shape: Shape,
+    val tooltip: String
+) {
     val outEdges = hashSetOf<Edge<T>>()
     val inEdges = hashSetOf<Edge<T>>()
     override fun toString() = content.toString()
